b153d57d3d0e40bb288f88ea77b48c08813c9fd5
pyroesp <nicolaslopezesp@gmail.com>
2026-02-18 00:18:31 +0100

    update thread files
    - added support for linux
    - updated thread structures, now with flags
    - updated findUnusedThread to make use of flags
    -update WaitUntilFinished to make use of flags

--- thread.h
+++ thread.h
@@ -1,8 +1,16 @@
 #ifndef _THREAD_H
 #define _THREAD_H
 
-#include <windows.h>
-#include <process.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include <process.h>
+#else
+    #define _POSIX_C_SOURCE 200809L
+    #include <pthread.h>
+    #include <signal.h>
+    #include <errno.h>
+#endif
 
 #include "file.h"
 #include "sha-256.h"
@@ -10,17 +18,37 @@
 // define max number of threads
 #define MAX_THREADS 16
 
+// platform-specific thread handle type
+#ifdef _WIN32
+    typedef HANDLE ThreadHandle;
+#else
+    typedef pthread_t ThreadHandle;
+#endif
+
 // thread structure to pass data to the thread
 typedef struct{
     s_File *f;
     s_SHA256_Digest *d;
-}s_Thread;
+}ThreadData;
 
-// thread function made as required by _beginthread
-void thread(void *p);
+// thread object containing handle, data and flags
+typedef struct{
+    ThreadHandle *handle;
+    ThreadData *data;
+    int running;
+    int finished;
+}ThreadObject;
+
+// thread function made as required by platform
+#ifdef _WIN32
+    void thread(void *p);
+#else
+    void* thread(void *p);
+#endif
+
 // return pointer to thread handle that is free
-HANDLE* thread_FindUnusedThread(HANDLE *t);
+int thread_FindUnusedThread(ThreadObject *to);
 // wait until all threads finished
-int thread_WaitUntilFinished(HANDLE *t);
+int thread_WaitUntilFinished(ThreadObject *to);
 
 #endif // _THREAD_H
